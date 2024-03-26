15e8a9eb1b5d41359763e08e04bd400dccc717e5
Sudre <aymeric.sudre@etu.uca.fr>
2024-03-26 16:43:42 +0100

    Potions + sorts + livres

--- Encyclopedie_HP/src/components/book.vue
+++ Encyclopedie_HP/src/components/book.vue
@@ -1,12 +1,102 @@
 <template>
     <div class="book-page">
-        <h1>Welcome to the book page!</h1>
+        <button class="home-button" @click="goToHome">Retour à l'accueil</button>
+        <h1>Page des livres</h1>
+        <input type="text" v-model="searchTerm" placeholder="Rechercher un livre" @change="fetchBooks">
+        <ul class="books-list">
+            <li class="book-item" v-for="book in books" :key="book.slug">
+                <h2 class="item-name"><span class="subtitle">{{ book.attributes.title }}</span></h2>
+                <img :src="book.attributes.cover" alt="Image du livre" v-if="book.attributes.cover">
+                <p class="item-info" v-else>Image non disponible</p>
+                <p class="item-info"><span class="subtitle">Auteur :</span> {{ book.attributes.author || 'Non disponible' }}</p>
+                <p class="item-info"><span class="subtitle">Date de sortie :</span>{{ book.attributes.release_date || 'Non disponible' }}</p>
+                <p class="item-info"><span class="subtitle">Résumé :</span>{{ book.attributes.summary || 'Non disponible' }}</p>
+                <p class="item-info"><span class="subtitle">Nombre de pages :</span>{{ book.attributes.pages || 'Non disponible' }}</p>
+                <p class="item-info"><span class="subtitle">Dédicace :</span>{{ book.attributes.dedication || 'Non disponible' }}</p>
+                <p class="item-info"><span class="subtitle">Lien Wiki :</span><a :href="book.wiki" target="_blank">Cliquez ici</a></p>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
     name: 'Book',
-    // Vous pouvez ajouter des données, des méthodes, des calculées, etc. ici
-}
+    data() {
+        return {
+            books: [],
+            searchTerm: '',
+        };
+    },
+    created() {
+        this.fetchBooks();
+    },
+    methods: {
+        fetchBooks() {
+            axios.get(`https://api.bookdb.com/v1/books?filter[name]=${this.searchTerm}`)
+                .then(response => {
+                    this.books = response.data.data;
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        },
+        goToHome() {
+            this.$router.push('/');
+        },
+    },
+};
 </script>
+
+<!-- Le reste du code reste inchangé -->
+
+<style scoped>
+p{
+    color: black;
+}
+h1{
+    color: black;
+}
+input{
+    background-color: #ffffff;
+    color: black;
+}
+.book-page {
+    padding: 20px;
+    background-color: #f9f9f9;
+}
+
+.books-list {
+    list-style-type: none;
+    padding: 0;
+    color: black;
+}
+
+.book-item {
+    margin-bottom: 20px;
+    padding: 20px;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+}
+
+.item-name {
+    margin: 0 0 10px;
+    font-size: 20px;
+}
+
+.item-info {
+    margin: 0 0 10px;
+    font-size: 16px;
+}
+.subtitle {
+        font-weight: bold;
+        text-decoration: underline;
+}
+.home-button {
+    float: left;
+    margin-right: -150px;
+    margin-top: 50px;
+}
+</style>
--- Encyclopedie_HP/src/components/spell.vue
+++ Encyclopedie_HP/src/components/spell.vue
@@ -1,12 +1,129 @@
 <template>
-    <div class="spell-page">
-      <h1>Welcome to the spell page!</h1>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'Spell',
-    // Vous pouvez ajouter des données, des méthodes, des calculées, etc. ici
-  }
-  </script>
+  <div class="spells-page">
+    <button class="home-button" @click="goToHome">Retour à l'accueil</button>
+    <h1>Page des sorts</h1>
+    <input type="text" v-model="searchTerm" placeholder="Rechercher un sort" @change="fetchSpells">
+    <ul class="spells-list">
+      <li class="spell-item" v-for="spell in spells" :key="potion.id">
+        <h2 class="item-name"><span class="subtitle">{{ spell.attributes.name }}</span></h2>
+        <img :src="spell.attributes.image" alt="Image du sort" v-if="spell.attributes.image">
+        <p class="item-info" v-else>Image non disponible</p>
+        <p class="item-info"><span class="subtitle">Catégorie :</span> {{ spell.attributes.category || 'Non disponible' }}</p>
+        <p class="item-info"><span class="subtitle">Effet :</span>{{ spell.attributes.effect || 'Non disponible' }}</p>
+        <p class="item-info"><span class="subtitle">Créateur :</span>{{ spell.attributes.creator || 'Non disponible' }}</p>
+      </li>
+    </ul>
+    <button @click="goToFirstPage" :disabled="page.number === 1"><<</button>
+    <button @click="previousPage" :disabled="page.number === 1"><</button>
+    <button @click="nextPage" :disabled="page.number === totalPages">></button>
+    <button @click="goToLastPage" :disabled="page.number === totalPages">>></button>
+    <br>
+    <input type="number" v-model.number="page.number" @change="fetchSpells" :min="1" :max="totalPages">
+    <p>Page actuelle : {{ page.number }} / {{ totalPages }}</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Spells',
+  data() {
+    return {
+      potions: [],
+      page: {
+        number: 1,
+        size: 25,
+      },
+      totalPages: 13,
+      searchTerm: '',
+    };
+  },
+  created() {
+    this.fetchSpells();
+  },
+  methods: {
+    fetchSpells() {
+      axios.get(`https://api.potterdb.com/v1/spells?page[number]=${this.page.number}&page[size]=${this.page.size}&filter[name]=${this.searchTerm}`)
+        .then(response => {
+          this.spells = response.data.data;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    },
+    nextPage() {
+      if (this.page.number < this.totalPages) {
+        this.page.number++;
+        this.fetchSpells();
+      }
+    },
+    previousPage() {
+      if (this.page.number > 1) {
+        this.page.number--;
+        this.fetchSpells();
+      }
+    },
+    goToFirstPage() {
+      this.page.number = 1;
+      this.fetchSpells();
+    },
+    goToLastPage() {
+      this.page.number = this.totalPages;
+      this.fetchSpells();
+    },
+    goToHome() {
+      this.$router.push('/');
+    },
+  },
+};
+</script>
+
+<style scoped>
+p{
+  color: black;
+}
+h1{
+  color: black;
+}
+input{
+  background-color: #ffffff;
+  color: black;
+}
+.spells-page {
+  padding: 20px;
+  background-color: #f9f9f9;
+}
+
+.spells-list {
+  list-style-type: none;
+  padding: 0;
+  color: black;
+}
+
+.spells-item {
+  margin-bottom: 20px;
+  padding: 20px;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+}
+
+.item-name {
+  margin: 0 0 10px;
+  font-size: 20px;
+}
+
+.item-info {
+  margin: 0 0 10px;
+  font-size: 16px;
+}
+.subtitle {
+    font-weight: bold;
+    text-decoration: underline;
+}
+.home-button {
+  float: left;
+  margin-right: -150px;
+  margin-top: 50px;
+}
+</style>
